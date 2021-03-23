cb6d4d3429878f24d1b3de15c2616059384621f8
chamberlainr4 <chamberlainr4@hwbcymru.net>
2021-03-23 14:05:26 +0000

    story

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
-    #write your code below this line
 
+    first = input('Hello we are going to write a story. Would you like to join me:')
+    second = input('Cool. What does the main character do:')
+    third = input('What is their name:')
+
+    print("Once upon a time there was a person called " + third + ", who ")
+    print("loved being a " + second + ".")
+    print(third + " lived happily ever after")
+
+   
 if __name__ == '__main__':
     main()
